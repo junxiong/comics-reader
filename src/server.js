df080cb8f6ca311d35bdcbb7c50b54c846710faa
Junxiong <shaojunxiong@gmail.com>
2015-10-08 18:39:32 +0800

    add db

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 import {fetchComic, searchComic} from './comic'
+import connect, {Histories} from './common/isomorphic/db'
 
-searchComic('血族')
-  .then(comics => console.log(comics))
+connect()
+  .then(collections => {
+    searchComic('血族')
+      .then(comics => console.log(comics))
+      .catch(error => console.log(error.message))
+
+    fetchComic('9924078')
+      .then(book => console.log(JSON.stringify(book)))
+      .catch('error', error => console.log(error))    
+  })
   .catch(error => console.log(error.message))
-
-fetchComic('9924078')
-  .then(book => console.log(JSON.stringify(book)))
-  .catch('error', error => console.log(error))
